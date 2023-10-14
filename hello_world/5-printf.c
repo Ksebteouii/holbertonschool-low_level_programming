a98f4617fc1f632329277955599cdfef3ff788be
Ksebteouii <sebteouiikhoubayb@gmail.com>
2023-10-14 15:26:55 -0700

    test this one

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- print a message
+* Return: 0
+ */
+int main(void) {
+printf("Art,");
+printf("but");
+printf("it");
+printf("is");
+printf("a");
+printf("piece");
+printf("of");
+printf("art,");
+printf("\n");
+
+return (0); 
+}

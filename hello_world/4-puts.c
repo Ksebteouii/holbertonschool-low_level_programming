e57d787b468b375c62e54753fa760b13a72d4442
Ksebteouii <sebteouiikhoubayb@gmail.com>
2023-10-14 15:10:37 -0700

    welcome c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This program prints a message about programming.
+ * Return: 0
+ */
+int main(void)
+{
+    printf("\"Programming is like building a multilingual puzzle\n");
+    return 0;
+}

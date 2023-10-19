48d1c66ae701afd4d3d790528ef27f2f74f701b6
sebteouii <sebteouiikhoubayb@gmail.com>
2023-10-19 10:10:53 +0200

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,27 +1,9 @@
 #include "main.h"
+int main (int argc, char *argv[]) {
+    int u;
 
-/**
- * Write a function that computes the absolute value of an integer.
- // Function to find the absolute value
-void findAbsolute(int N)
-{
- 
-    // If the number is less than
-    // zero, then multiply by (-1)
-    if (N < 0) {
-        N = (-1) * N;
-    }
-       // Print the absolute value
-    printf("%d ", N);
+    scanf("%d", &u);
+    printf("absValue = %u\n", u);
+
+return 0;
 }
-// Driver Code
-int main()
-{
- 
-    // Given integer
-    int N = -12;
-    // Function call
-    findAbsolute(N);
-    return 0;
-}
- 

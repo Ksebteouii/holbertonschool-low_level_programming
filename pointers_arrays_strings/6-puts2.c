4259d9d320e1b8d0486872276c9cbc53e0a8132c
sebteouii <sebteouiikhoubayb@gmail.com>
2023-10-24 14:28:39 +0200

    Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * description :
+ * @s: The string to be modified
+ * Return: void
+ */
+int main(void) int i;
+char word;
+char input;
+
+for (i = 0; i < strlen(input); i += 2)
+{
+	{
+		word[i] += input[i];
+	}
+	printf("\n\n2nd option\n%s", s);
+}

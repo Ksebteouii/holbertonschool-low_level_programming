9c85786613cb49118a6d190aaab33439ac51b366
sebteouii <sebteouiikhoubayb@gmail.com>
2023-10-28 22:29:21 +0200

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,19 +2,42 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * string_toupper - function that reverses a string.
- * Return: upp
+ * cap_string - function that capitalizes all words of a string
+ * Return: str
  * @str : pointeur
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i;
+	char *p = str;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = (int)str[i] - 32;
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = (int)str[i] - 32;
+		}
+		switch (str[i])
+		{
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
 	}
-	return (str);
+	return (p);
 }
- 

e20d5f9f7ee1fbb9634596b7024bd7c254d81471
sebteouii <sebteouiikhoubayb@gmail.com>
2023-10-30 10:03:59 +0100

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memse - function that copies me 
+ * return: s
+ * @s :pointeur
+ * @b :pointeur
+ * @n :pointeur 
+ * /
+ * char *_memset(char *s, char b, unsigned int n);
+ * {
+ * unsigned int n ;
+ * for (i=s[i],i<n,i=++)
+ * s[i]=i
+ * return(s)
+ * }
+ * 

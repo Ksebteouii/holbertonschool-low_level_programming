4166ec5ad2008917aa7626fd51799f95a6ae01e3
sebteouii <sebteouiikhoubayb@gmail.com>
2023-10-30 10:56:16 +0100

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_memcpy - function that copies m
+ * @dest :pointeur
+ * @src :pointeur
+ * @n :nbr of byte
+ * return : to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}

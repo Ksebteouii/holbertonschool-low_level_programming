27504abd2f728ca30deb3cde452bef19963b37ed
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-02 10:06:52 +0100

    You mustn't be afraid to dream a little bigger, darling

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * Factorial-function that returns the factorial of a given number
+ * @n:nbr
+ * return: 1,-1,f
+ */
+int factorial(int n);
+{
+	int f;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	f = n * factorial(n - 1);
+	return (f);
+}

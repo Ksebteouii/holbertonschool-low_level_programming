ebb7130aab76c5c6cb5a8ba3f3a9d9b54a47604c
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-08 11:07:27 +0100

    fnct that allocates memory using malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ malloc_checked-function that allocates memory using malloc
+ * @b:integer
+ * Return: size allocated memorry
+ */
+void *malloc_checked(unsigned int b);
+{
+	void *b = malloc(b);
+	if (n == NULL)
+		exist(98);
+	return (0);
+}

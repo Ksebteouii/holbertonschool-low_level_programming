b99c0bf5bfb31e08ad5382147bbb427a224cc0a2
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-13 11:29:12 +0100

    define new structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef DOG
 #define DOG
-
+/**
+ * struct dog -type struct
+ * @name : name of dog
+ * @age : age of dog
+ * @owner : owner of dog
+ */
 
 typedef struct dog
 {
-char * name
-float age 
-char * owner
-}dog;
+	char *name;
+	float age;
+	char *owner;
+} dog;
 
 #endif

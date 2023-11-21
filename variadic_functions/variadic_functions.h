66632fe9a791ad0c473d89617a3b960c61839d67
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-21 20:54:53 +0100

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void convert_char(va_list argm);
+void convert_int(va_list argm);
+void convert_float(va_list argm);
+void convert_string(va_list argm);
+void print_all(const char *const format, ...);
 #endif

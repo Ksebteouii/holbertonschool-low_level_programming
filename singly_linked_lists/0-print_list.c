76845f8b60541a39433cec3592a3b79dc68333fc
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-30 12:04:32 +0100

    fnction that print all the list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,8 +4,7 @@ size_t print_list-function that prints all the elements of a list_t list
 @h::
 Return: nombre des element
 */
-size_t print_list(const list_t *h);
-
+size_t print_list(const list_t *h)
 {
     unsigned int i;
 
